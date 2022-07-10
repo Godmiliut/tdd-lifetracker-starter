d9ad684ebb62464a99a83f30aadae5ff3c0df2ee
Godmiliut <supermiliut72@gmail.com>
2022-07-10 14:47:55 -0400

    changed API_BASE_URL

--- lifetracker-ui/src/constants.js
+++ lifetracker-ui/src/constants.js
@@ -2,7 +2,7 @@ const PRODUCTION_API_BASE_URL= "";
 
 const DEVELOPMENT_API_BASE_URL= "http://localhost:3001"
 
-const API_BASE_URL= process.env.NODE_ENV == "production" ? PRODUCTION_API_BASE_URL : DEVELOPMENT_API_BASE_URL 
+const API_BASE_URL= "https://lifetracker-gbb.herokuapp.com/"
 
 module.exports = {
     PRODUCTION_API_BASE_URL,
